f762c20f159a70a615df2e746e6fc16ed63b2b09
Mikhail Butuzov <mikhailbish@gmail.com>
2023-01-19 10:27:10 +0200

    remove deprecation warning

--- src/plugins/db/index.js
+++ src/plugins/db/index.js
@@ -1,5 +1,6 @@
 
 const connect = async ( mongoose, connectionString, schema, modelName, dbName ) => {
+  mongoose.set('strictQuery', true);
   const connection = await mongoose.connect( connectionString );
   const dbDataSchema = new mongoose.Schema( schema );
   const model = connection.model( modelName, dbDataSchema, dbName );
